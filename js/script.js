318aaa7a3f92486882d2f02c62171fe85815ec7d
Tema <130843262+TemaMel@users.noreply.github.com>
2023-09-28 00:01:16 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@ window.addEventListener('scroll', e => {
     document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
 })
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-ScrollSmoother.create({
-	wrapper: '.wrapper',
-	content: '.content'
-})
+// gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+// ScrollSmoother.create({
+// 	wrapper: '.wrapper',
+// 	content: '.content'
+// })
 
 
 const hamburger = document.querySelector('.hamburger'),
 	menu = document.querySelector('.menu'),
 	closeElem = document.querySelector('.menu__close');
 	// menuLink = document.querySelector('.menu__link');
-	menuLink = document.getElementById('link')
+	menuLink = document.querySelector('.menu__link');
 
 hamburger.addEventListener('click', function() {
 	menu.classList.add('active');
@@ -24,15 +24,25 @@ closeElem.addEventListener('click', function() {
 });
 
 menuLink.addEventListener('click', function() {
+	(this)
 	menu.classList.remove('active');
 })
 
 
 
+/////Section Skills //////
 
 const counters = document.querySelectorAll('.skills__ratings-counter'),
       lines = document.querySelectorAll('.skills__ratings-line span');
 
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
+});
+
+/////AOS INIT/////
+
+
+AOS.init({
+	disable: 'phone',
+	duration: 2000,
 });
